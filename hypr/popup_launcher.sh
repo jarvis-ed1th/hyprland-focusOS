e0b91f682c6c313ebf4551544dee721fadbf8a13
ben <jarvis.edith85@gmail.com>
2025-12-13 18:59:00 +0100

    fix: fix behavior for waybar setting windows

--- hypr/popup_launcher.sh
+++ hypr/popup_launcher.sh
@@ -20,8 +20,10 @@ hyprctl keyword input:float_switch_override_focus 0
 $@ &
 PID=$!
 
+
 # On attend un instant que la fenêtre apparaisse et prenne le focus
 sleep 0.2
+ORIGIN_TITLE=$(hyprctl activewindow -j | jq -r '.title')
 
 # 3. BOUCLE DE SURVEILLANCE
 while true; do
@@ -31,11 +33,11 @@ while true; do
     fi
 
     # Quelle est la fenêtre qui a le focus ACTUELLEMENT ?
-    ACTIVE_CLASS=$(hyprctl activewindow -j | jq -r '.class')
+    ACTIVE_TITLE=$(hyprctl activewindow -j | jq -r '.title')
 
     # Si la fenêtre active n'est PLUS notre popup "dotfiles-floating"
     # C'est que l'utilisateur a CLIQUÉ ailleurs (car le survol est désactivé)
-    if [[ "$ACTIVE_CLASS" != "dotfiles-floating" ]]; then
+    if [[ "$ACTIVE_TITLE" != "$ORIGIN_TITLE" ]]; then
         # On ferme la fenêtre proprement
         kill $PID
         break
